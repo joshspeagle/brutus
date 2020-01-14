fix automatic masking
Fixed a logic mistake when automatically masking objects. Added a new `mag_max` option so that the default of `50` is now adjustable.

brutus/fitting.py
@@ -605,7 +605,7 @@ class BruteForce():
             lngalprior=None, lndustprior=None, dustfile=None,
             apply_dlabels=True, data_coords=None, logl_dim_prior=True,
             ltol=3e-2, ltol_subthresh=1e-2, logl_initthresh=5e-3,
-            merr_min=0.25, rstate=None, save_dar_draws=True,
+            mag_max=50., merr_min=0.25, rstate=None, save_dar_draws=True,
             running_io=True, verbose=True):
         """
         Fit all input models to the input data to compute the associated
@@ -738,6 +738,10 @@ class BruteForce():
             set of fits but before optimizing them. Default is `5e-3`.
             **This must be smaller than or equal to `ltol_subthresh`.**
 
+        mag_max : float, optional
+            The maximum allowed magnitude (converted from the provided
+            fluxes) used for internal masking. Default is `50.`.
+
         merr_min : float, optional
             The minimum allowed magnitude error (converted from the provided
             fluxes) used for internal masking. Default is `0.25`.
@@ -847,7 +851,7 @@ class BruteForce():
         with warnings.catch_warnings():
             warnings.simplefilter("ignore")
             mag, err = magnitude(data, data_err)
-            bad_mag = (mag > 50) & (err < merr_min)
+            bad_mag = (mag > mag_max) | (err > merr_min)
             clean = np.isfinite(data) & np.isfinite(data_err) & (data_err > 0.)
             data_mask *= (clean & ~bad_mag)
 
@@ -858,7 +862,7 @@ class BruteForce():
                              "acceptable photometry are currently included in "
                              "the dataset. These objects give degenerate fits "
                              "and cannot be properly modeled. Please remove "
-                             "these objects or increase `merr_min`."
+                             "these objects or modify `mag_max` or `merr_min`."
                              .format(Nbmin))
 
         # Initialize results file.